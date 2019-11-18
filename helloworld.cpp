bb877bd39d0e4b10c190d6d7a21215ee72373f6a
Peter Winer <winer.peter@gmail.com>
2019-11-18 16:33:49 +0000

    For you Harry

--- helloworld.cpp
+++ helloworld.cpp
@@ -1,17 +1,58 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include <stdlib.h>
 
 using namespace std;
 
-int main()
+struct text;
+typedef struct text text;
+
+struct text
+{
+    int capacity;
+    char *content;
+};
+
+// Create a new text structure, containing a copy of the given string.
+text *newText(char *s)
+{
+    int size = 24;
+
+    while (strlen(s) >= size)
+        size *= 2;
+
+    text *t = (text *)malloc(sizeof(text));
+
+    *t = (text){size, (char *)malloc(size)};
+
+    strcpy(t->content, s);
+    return t;
+}
+
+void append(text *t1, text *t2)
 {
 
-    vector<string> msg{"Hello", "C++", "World", "from", "VS Code!"};
+    int size = 24;
+    int targetSize = strlen(t1->content) + strlen(t2->content);
 
-    for (const string &word : msg)
-    {
-        cout << word << " ";
-    }
-    cout << endl;
+    while (targetSize >= size)
+        size *= 2;
+
+    t1->content = (char *)realloc(t1->content, size);
+    t1->capacity = size;
+    strcat(t1->content, t2->content);
+}
+
+int main()
+{
+    char s[100];
+
+    strcpy(s, "Hello Boozer");
+    text *t1 = newText(s);
+
+    strcpy(s, " we love you, Boozer");
+    text *t2 = newText(s);
+
+    append(t1, t2);
 }
